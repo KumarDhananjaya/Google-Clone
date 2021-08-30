47ca3e6c481d707266315172e3ee049c4d353fb2
Kumar Dhananjaya <kumar62.shivu@gmail.com>
2021-08-30 11:32:15 +0530

    updated new file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello all, Welcome</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;

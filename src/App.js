69418fbb0ac5b3c36da5261c641305718c9054c0
Kumar Dhananjaya <kumar62.shivu@gmail.com>
2021-08-30 15:23:30 +0530

    aded home page and router for several pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
-import Home from './pages/Home';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello all, Welcome</h1>
-      <Home />
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1> This is the search page</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps";
+import { Avatar } from "@material-ui/core";
 
 function Home() {
-    return (
-        <div className="home">
-            <h1> This is Home page </h1>
-            
+  return (
+    <div className="home">
+      <div className="home_header">
+        <div className="home_headerLeft">
+          <Link to="/about">About</Link>
+          <Link to="/store">Store</Link>
         </div>
-    )
+        <div className="home_headerRight">
+          <Link to="/gmail">Gmail</Link>
+          <Link to="/images">Images</Link>
+          <AppsIcon />
+          <Avatar />
+        </div>
+      </div>
+
+      <div className="home_body">
+        <img src="https://cdn.vox-cdn.com/thumbor/8tLchaDMIEDNzUD3mYQ7v1ZQL84=/0x0:2012x1341/920x613/filters:focal(0x0:2012x1341):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg" />
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
